473316b0299bcb69cb719baee2c8bc1b8009aa12
Pedro <pedro.cruz@ibm.com>
2020-04-01 17:57:12 -0400

    refactor: allow for multi-part data in POST

--- api/case/controllers/case.js
+++ api/case/controllers/case.js
@@ -1,8 +1,20 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.case.create(data, { files });
+    } else {
+      entity = await strapi.services.case.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.case });
+  },
+};
